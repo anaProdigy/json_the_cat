d6399f66f0504f1964b351c7b1776487dd8e34b6
Anastasia <z.anastasia11@gmail.com>
2023-03-26 16:07:30 -0700

    create index.js and move process.argv there, call fetchBreedDescrition from index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,49 @@
 const request = require('request');
 
-const breedName = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
 
-  if (error) {
-    console.log(`Failed to request details: ${error}`);
-  } else if (response.statusCode !== 200) {
-    console.log(`Invalid response status code ${response.statusCode} fpr ${url}`);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-})
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(url, (error, response, body) => {
+
+    if (error) {
+      console.log(`Failed to request details: ${error}`);
+    } else if (response.statusCode !== 200) {
+      console.log(`Invalid response status code ${response.statusCode} fpr ${url}`);
+    } else {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
+
+// const fetchBreedDescription = function(breedName, callback) {
+//   request(url, (error, response, body) => {
+//     //console.log(typeof body);
+//     if (error) {
+//       console.log(`Failed to request details: ${error}`, null);
+//     } else if (response.statusCode !== 200) {
+//       console.log(`Invalid response status code ${response.statusCode} for ${url}`);
+//     } else {
+//       fs.writeFile(filePath, body, (err) => {
+//         if (err) {
+//           console.log(`Error saving to ${filePath}: ${err}`);
+//         } else {
+//           console.log(`Downloaded and saved  `);
+//         }
+
+//         const data = JSON.parse(body);
+//         // console.log(data);
+//         // console.log(typeof data);
+//       });
+//     }
+//   });
+
+// };
 
 
 
